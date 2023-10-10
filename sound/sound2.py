6fb4a91353efc4dc375842bff08f1efe7cdb8db5
tizer <72142468+tizerr@users.noreply.github.com>
2023-10-10 19:45:42 +0300

    Delete sound directory

--- sound/sound2.py
+++
@@ -1,8 +0,0 @@
-import wave
-
-with wave.open('sample.wav', 'rb') as src:
-    data = src.readframes(src.getnframes())
-    new_data = data[::-1]
-with wave.open('out.wav', 'wb') as out:
-    out.setparams(src.getparams())
-    out.writeframes(new_data)
--- sound/sound3.py
+++
@@ -1,8 +0,0 @@
-import wave
-
-with wave.open('sample.wav', 'rb') as src:
-    data = src.readframes(src.getnframes())
-    new_data = [i * 2 for i in data]  # увеличение
-with wave.open('out.wav', 'wb') as out:
-    out.setparams(src.getparams())
-    out.writeframes(new_data)
